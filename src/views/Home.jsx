f9b8430624dfe03424ff776594fc32cb1a3746a8
Hanssel Jacobo Miranda Díaz <hansselmiranda1@gmail.com>
2025-11-08 11:43:03 -0600

    Mostrar FirestoreDemo en Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Titulo from "../components/Titulo";
+import Contador from "../components/Contador";
+import FirestoreDemo from "../components/FirestoreDemo";
 
 const Home = () => {
   return (
-    <>
-      <div style={{ padding: 20 }}>
-        <Titulo texto="Bienvenido a mi Proyecto React con Vite" />
-        <p>Este es el contenido principal de la página de inicio.</p>
-      </div>
-    </>
+    <div style={{ padding: 20 }}>
+      <Titulo texto="Bienvenido a mi Proyecto React con Vite" />
+      <p>Este es el contenido principal de la página de inicio.</p>
+
+      <hr />
+
+      <Contador />
+
+      <hr />
+
+      {/* Aquí mostramos el demo de Firestore */}
+      <FirestoreDemo />
+    </div>
   );
 };
 
